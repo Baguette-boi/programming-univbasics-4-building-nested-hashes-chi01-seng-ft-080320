e1762ec3c7fa4e2a8ae37ed816c5eaf916ed23b9
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-19 20:23:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,7 @@ monopoly = {
       },
       :names => {
         :reading_railroad => {
-          "mortgage_value" => 100
+          :mortgage_value => 100
         },
         :pennsylvania_railroad =>{
           "mortgage_value" => "$200"
